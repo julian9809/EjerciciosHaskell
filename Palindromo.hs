8c5d7898d711895159e38a6a4c2071d6f6217c26
julian9809 <juliand9809@gmail.com>
2017-09-11 17:43:36 -0500

    Add files via upload

--- Palindromo.hs
+++ Palindromo.hs
@@ -1,6 +1,5 @@
 palindromo::Int->Int
-palindromo 0 = 1 
-palindromo a = if a<10 then 1 else if mod a 10 == primero a then palindromo (mod (a-ultimo a) 10) else 0
+palindromo a = if a<10 then 1 else if mod a 10 == primero a then palindromo (div (a-(ultimo a)) 10) else 0
 
 ultimo::Int->Int 
 ultimo a = if a<10 then a else (ultimo (div a 10))*10
