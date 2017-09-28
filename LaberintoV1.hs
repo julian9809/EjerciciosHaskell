7d6c049c1665b957443abe150e6415965b2e6732
julian9809 <juliand9809@gmail.com>
2017-09-28 00:26:15 -0500

    Encuentra el inicio

--- LaberintoV1.hs
+++ LaberintoV1.hs
@@ -3,13 +3,13 @@ module Laberinto where
 matxy :: [[Char]]->(Int,Int)->Char
 matxy a (fila,columna) =  (a!!fila)!!columna
 
-mover::(Int,Int)->(Int,Int)
-mover (fila,columna) = if (columna < 2) then (fila,columna+1) else if (columna == 2) then (fila+1,0) else (fila,columna)
+mover:: [[Char]]->(Int,Int)->(Int,Int)
+mover (a:as) (fila,columna) = if (columna < ((length a)-1)) then (fila,columna+1) else if (columna == ((length a)-1)) then (fila+1,0) else (fila,columna)
 
 encontrarInicio:: [[Char]]->(Int,Int)->(Int,Int)
 encontrarInicio a (fila,columna)
     | matxy a (fila,columna) == 'x' = (fila,columna)
-    | matxy a (fila,columna) /= 'x' = encontrarInicio a (mover(fila,columna))  
+    | matxy a (fila,columna) /= 'x' = encontrarInicio a (mover a (fila,columna))  
 
 
 
